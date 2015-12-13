29dec47271b6a417f985d20917b1448c8376a27f
thiar <thiar.hasbiya@gmail.com>
2015-12-13 14:23:17 +0700

    update

--- application/controllers/C_User.php
+++ application/controllers/C_User.php
@@ -3,21 +3,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class C_User extends CI_Controller {
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see http://codeigniter.com/user_guide/general/urls.html
-	 */
 	public function __construct()
     {
         // Call the CI_Model constructor
@@ -35,12 +20,13 @@ class C_User extends CI_Controller {
 		        'nama' => $this->input->post('nama'),
 		        'password' => sha1($this->input->post('password')),
 		        'hak_akses' => $this->input->post('hak_akses'),
-		        'create_at' => 'NOW()',
+		        'create_at' => date('Y-m-d'),
 		        'delete_at' => 'NULL',
-		        'update_at' => 'NOW()',
+		        'update_at' => date('Y-m-d'),
 		        'no_hp' => $this->input->post('no_hp'),
 		        'email' => $this->input->post('email')
 		);
+		// echo date('Y-m-d');
 		echo $this->M_User->add($data);
 	}
 }
